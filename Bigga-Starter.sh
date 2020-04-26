6070852ea21b00f2eea02cccf1337809832db6de
TMP-tenpura <56856373+TMP-tenpura@users.noreply.github.com>
2020-04-26 18:36:32 +0900

    アイコンを表示できるようZenityに引数を追加

--- Bigga-Starter.sh
+++ Bigga-Starter.sh
@@ -2,6 +2,7 @@
 if [[ -e /etc/calamares ]]; then
   exit
 fi
+icon="/etc/micno/bigga.svg"
 if [[ -e ~/.config/autostart/biggastarter.desktop ]]; then
   rm ~/.config/autostart/biggastarter.desktop
 fi
@@ -10,9 +11,14 @@ if [ ! "$UID" -eq 0 ];then
   pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY bash `cd $(dirname ${0}) && pwd`/`basename $0`
   exit
 fi
-zenity --info --width=400 --title BiggaStarter --text 'これから簡単な操作を行います。インターネットに接続したことを確認してからOKを押してください。'
+if [[ ! -e /etc/micno/Bigga-Starter.sh ]]; then
+  mkdir /etc/micno/
+  cp $0 /etc/micno/Bigga-Starter.sh
+  cp ./bigga.svg /etc/micno/bigga.svg
+fi
+zenity --info --width=400 --window-icon=$icon --title BiggaStarter --text 'これから簡単な操作を行います。インターネットに接続したことを確認してからOKを押してください。'
 while ! ping -q -c 1 -W 1 google.com >/dev/null; do
-  zenity --info --width=400 --title "ネットワークエラー" --text "ネットワークに接続して、OKを押してください。"
+  zenity --info --width=400 --window-icon=$icon --title "ネットワークエラー" --text "ネットワークに接続して、OKを押してください。"
 done
 if [[ ! `cat /etc/apt/sources.list` = *"deb http://deb.debian.org/debian stable main"*"deb-src http://deb.debian.org/debian stable main"* ]];then
   (
@@ -27,13 +33,10 @@ EOF
   zenity --progress \
     --title="初期設定中..."\
     --width=400\
+    --window-icon=$icon\
     --text="/etc/apt/sources.listを編集中..."\
     --pulsate\
     --auto-close
-fi
-if [[ ! -e /etc/micno/Bigga-Starter.sh ]]; then
-  mkdir /etc/micno/
-  cp $0 /etc/micno/Bigga-Starter.sh
 fi
 if [[ ! -e /usr/share/applications/biggastarter.desktop ]]; then
   cat <<EOF > /usr/share/applications/biggastarter.desktop
@@ -49,7 +52,7 @@ Categories=System;
 StartupNotify=true
 EOF
 fi
-browser=`zenity --list --title BiggaStarter --text=インストールするブラウザを選択してください。 --radiolist --column="" --column="ブラウザ名" "" "Chrome" "" "Vivaldi" "" "Firefox(ESR)" "" "Chromium"`
+browser=`zenity --list --window-icon=$icon --title BiggaStarter --text=インストールするブラウザを選択してください。 --radiolist --column="" --column="ブラウザ名" "" "Chrome" "" "Vivaldi" "" "Firefox(ESR)" "" "Chromium"`
 if [[ $browser = 'Firefox(ESR)' ]]; then
   (
   echo "10" ; apt-get install -y firefox-esr
@@ -60,6 +63,7 @@ if [[ $browser = 'Firefox(ESR)' ]]; then
   zenity --progress \
     --title="Firefoxをインストール中..."\
     --width=400\
+    --window-icon=$icon\
     --text="apt-get install firefox-esrを実行中..."\
     --percentage=0\
     --auto-close
@@ -73,6 +77,7 @@ elif [[ $browser = 'Chromium' ]]; then
   zenity --progress \
     --title="Chromiumをインストール中..."\
     --width=400\
+    --window-icon=$icon\
     --text="apt-get install chromiumを実行中..."\
     --percentage=0\
     --auto-close
@@ -87,6 +92,7 @@ elif [[ $browser = 'Chrome' ]]; then
   ) |
   zenity --progress \
     --width=400\
+    --window-icon=$icon\
     --title="Chromeをインストール中..."\
     --text="ネットワークからdebファイルをダウンロード中..."\
     --percentage=0\
@@ -103,17 +109,18 @@ elif [[ $browser = 'Vivaldi' ]]; then
   zenity --progress \
     --title="ViValdiをインストール中..."\
     --width=400\
+    --window-icon=$icon\
     --text="ネットワークからdebファイルをダウンロード中..."\
     --percentage=0\
     --auto-close
 fi
-zenity --question --width=400 --title BiggaStarter --text="コミュニケーションツール(Skypeなど)をインストールしますか？" 2>/dev/null
+zenity --question --width=400 --window-icon=$icon --title BiggaStarter --text="コミュニケーションツール(Skypeなど)をインストールしますか？" 2>/dev/null
 EXITCODE=$?
 if [[ $EXITCODE = 0 ]]; then
   if [[ $browser = 'Firefox(ESR)' ]]; then
-    tool=`zenity --list --title BiggaStarter --width=400 --height=200 --text=インストールするコミュニケーションツールを選択してください。 --checklist --column="" --column="ツール名" "" "Skype" "" "Discord" "" "Slack" "" "Hangouts"`
+    tool=`zenity --list --title BiggaStarter　--window-icon=$icon --width=400 --height=200 --text=インストールするコミュニケーションツールを選択してください。 --checklist --column="" --column="ツール名" "" "Skype" "" "Discord" "" "Slack" "" "Hangouts"`
   else
-    tool=`zenity --list --title BiggaStarter --width=400 --height=200 --text=インストールするコミュニケーションツールを選択してください。 --checklist --column="" --column="ツール名" "" "Skype" "" "Discord" "" "Slack" "" "LINE" "" "Hangouts"`
+    tool=`zenity --list --title BiggaStarter　--window-icon=$icon --width=400 --height=200 --text=インストールするコミュニケーションツールを選択してください。 --checklist --column="" --column="ツール名" "" "Skype" "" "Discord" "" "Slack" "" "LINE" "" "Hangouts"`
   fi
   if [[ `echo $tool | grep Skype` ]];then
     (
@@ -127,6 +134,7 @@ if [[ $EXITCODE = 0 ]]; then
     zenity --progress \
       --title="Skypeをインストール中..."\
       --width=400\
+      --window-icon=$icon\
       --text="ネットワークからdebファイルをダウンロード中..."\
       --percentage=0\
       --auto-close
@@ -143,6 +151,7 @@ if [[ $EXITCODE = 0 ]]; then
     zenity --progress \
       --title="Discordをインストール中..."\
       --width=400\
+      --window-icon=$icon\
       --text="ネットワークからdebファイルをダウンロード中..."\
       --percentage=0\
       --auto-close
@@ -159,6 +168,7 @@ if [[ $EXITCODE = 0 ]]; then
     zenity --progress \
       --title="Slackをインストール中..."\
       --width=400\
+      --window-icon=$icon\
       --text="ネットワークからdebファイルをダウンロード中..."\
       --percentage=0\
       --auto-close
@@ -233,10 +243,10 @@ EOF
     fi
   fi
 fi
-zenity --question --width=400 --title BiggaStarter --text="クラウドストレージサービス(DropBoxなど)をインストールしますか？" 2>/dev/null
+zenity --question --width=400 --window-icon=$icon --title BiggaStarter --text="クラウドストレージサービス(DropBoxなど)をインストールしますか？" 2>/dev/null
 EXITCODE=$?
 if [[ $EXITCODE = 0 ]]; then
-  storage=`zenity --list --title BiggaStarter --text=インストールするコミュニケーションツールを選択してください。 --checklist --column="" --column="ツール名" "" "Dropbox" "" "MEGA"`
+  storage=`zenity --list --window-icon=$icon --title BiggaStarter --text=インストールするコミュニケーションツールを選択してください。 --checklist --column="" --column="ツール名" "" "Dropbox" "" "MEGA"`
   if [[ `echo $storage | grep Dropbox` ]];then
     (
     echo "10" ; wget --trust-server-names "https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2020.03.04_amd64.deb"  ; filename=`find dropbox*.deb`
@@ -249,6 +259,7 @@ if [[ $EXITCODE = 0 ]]; then
     zenity --progress \
       --title="Dropboxをインストール中..."\
       --width=400\
+      --window-icon=$icon\
       --text="ネットワークからdebファイルをダウンロード中..."\
       --percentage=0\
       --auto-close
@@ -265,11 +276,11 @@ if [[ $EXITCODE = 0 ]]; then
     zenity --progress \
       --title="MEGAをインストール中..."\
       --width=400\
+      --window-icon=$icon\
       --text="ネットワークからdebファイルをダウンロード中..."\
       --percentage=0\
       --auto-close
   fi
 fi
 
-zenity --info --width=400 --title すべての操作が完了しました --text すべての操作が完了しました。さあ、MicnoLinuxを使い始めましょう! 
-
+zenity --info --width=400　--window-icon=$icon --title すべての操作が完了しました --text すべての操作が完了しました。さあ、MicnoLinuxを使い始めましょう! 
